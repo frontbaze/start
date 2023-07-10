98dd396e9c67ab1817196db07c6cf4455f371c03
Anton Vanjukhin <frontbaze@gmail.com>
2023-07-10 22:06:05 +1000

    fixed useTheme & translations

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import Button from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -16,6 +17,8 @@ export const Sidebar = ({ className }: SidebarProps) => {
     setCollapsed((prev) => !prev);
   };
 
+  const { t } = useTranslation();
+
   return (
     <div
       data-testid="sidebar"
@@ -23,9 +26,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className as string,
       ])}
     >
-      {/* <Button data-testid="toggle-sidebar" onClick={onToggle}>
-        toggle
-      </Button> */}
+      <Button data-testid="toggle-sidebar" onClick={onToggle}>
+        {t('toggle')}
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
