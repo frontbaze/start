bb432cdd8a31ac39b92273eeda74a1fdcbb8ba7c
Anton Vanjukhin <frontbaze@gmail.com>
2023-07-10 15:34:08 +1000

    fix comment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-// eslint-disable i18next/no-literal-string
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
