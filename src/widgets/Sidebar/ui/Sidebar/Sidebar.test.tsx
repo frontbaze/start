70a23c7e75046e85783156ae99de06b784deb892
Anton Vanjukhin <frontbaze@gmail.com>
2023-07-12 13:00:34 +1000

    add tests with routes

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import {
-  renderWithTranslation
-} from 'shared/lib/classNames/tests/renderWithTranslation/renderWithTranslation';
+  componentRender,
+} from 'shared/lib/classNames/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('render sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleButton = screen.getByTestId('toggle-sidebar');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleButton);
